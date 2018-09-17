ef3f08db8a62d6ec708ca4135b011bc59ce9b153
Nicolas Meier <ws@nclsmr.com>
2018-09-17 10:33:45 +0200

    Update deploy.sh script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -
-cp get_netrc.py ~/Library/Scripts/Applications/
-cp openssl_encrypt.sh ~/Library/Scripts/Applications/
+cp get_netrc.py ~/Scripts/Applications/
+cp openssl_encrypt.sh ~/Scripts/Applications/
